9a2f028e4ef00bd51c095d609ce65608f97c5845
Felipe Kessi <kessi@koders.cl>
2022-08-01 22:59:00 -0400

    final clase colections

--- src/cl/fsj/infoclub/colecttions/Colecciones1.java
+++ src/cl/fsj/infoclub/colecttions/Colecciones1.java
@@ -2,7 +2,13 @@ package cl.fsj.infoclub.colecttions;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.IllegalFormatPrecisionException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Colecciones1 {
 	
@@ -17,6 +23,7 @@ public class Colecciones1 {
 		decimales.add(12.456);
 		decimales.add(32.34);
 		decimales.add(62.4);
+		decimales.add(32.4);
 		
 		System.out.println("Estos son los decimales de la lista decimales: "+decimales);
 		System.out.println("*************************");
@@ -31,7 +38,113 @@ public class Colecciones1 {
 		decimales.addAll(deci);
 		System.out.println("**************************");
 		System.out.println(decimales);
+		
 		System.out.println(decimales.size());
+		
+		System.out.println(decimales.indexOf(32.4)); //0
+		System.out.println(decimales.lastIndexOf(32.4)); //0
+		System.out.println(decimales.lastIndexOf(32.34));//3
+		System.out.println(decimales.lastIndexOf(32.4456));//-1
+		
+		/*System.out.println("*********SET*********");
+		Set<String> nicknames = new HashSet<>();
+		
+		nicknames.add("ducksito");
+		nicknames.add("rolando90");
+		nicknames.add("polypleti");
+		nicknames.add("gonzalezlillo");
+		nicknames.add("edgamer63");
+		nicknames.add("silvia2015");
+		nicknames.add("sebaoteiza");
+		nicknames.add("callita");
+		nicknames.add("giornobanana");
+		nicknames.add("lucianocesc");
+		nicknames.add("felicity");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		nicknames.add("kessillox");
+		
+		System.out.println(nicknames);*/
+		
+		/*System.out.println("*********LinkedHashSet*********");
+		Set<String> nicknames = new LinkedHashSet<>();
+		
+		nicknames.add("ducksito");
+		nicknames.add("rolando90");
+		nicknames.add("polypleti");
+		nicknames.add("gonzalezlillo");
+		nicknames.add("edgamer63");
+		nicknames.add("silvia2015");
+		nicknames.add("sebaoteiza");
+		nicknames.add("callita");
+		nicknames.add("giornobanana");
+		nicknames.add("lucianocesc");
+		nicknames.add("felicity");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		nicknames.add("kessillox");
+		
+		System.out.println(nicknames);*/
+		
+		/*System.out.println("*********TreeSet*********");
+		Set<String> nicknames = new TreeSet<>();
+		
+		nicknames.add("ducksito");
+		nicknames.add("rolando90");
+		nicknames.add("polypleti");
+		nicknames.add("gonzalezlillo");
+		nicknames.add("edgamer63");
+		nicknames.add("silvia2015");
+		nicknames.add("sebaoteiza");
+		nicknames.add("callita");
+		nicknames.add("giornobanana");
+		nicknames.add("lucianocesc");
+		nicknames.add("felicity");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		nicknames.add("kessillox");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		
+		System.out.println(nicknames);*/
+		
+		System.out.println("*********QUEUE*********");
+		Queue<String> nicknames = new LinkedList<>();
+		
+		nicknames.add("ducksito");
+		nicknames.add("rolando90");
+		nicknames.add("polypleti");
+		nicknames.add("gonzalezlillo");
+		nicknames.add("edgamer63");
+		nicknames.add("silvia2015");
+		nicknames.add("sebaoteiza");
+		nicknames.add("callita");
+		nicknames.add("giornobanana");
+		nicknames.add("lucianocesc");
+		nicknames.add("felicity");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		nicknames.add("kessillox");
+		nicknames.add("kernelweb");
+		nicknames.add("cr111");
+		
+		System.out.println(nicknames);
+
+
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 	
 
